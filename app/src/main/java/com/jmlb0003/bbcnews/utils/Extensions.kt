ebc1f7d1a7b8f0551fb1b8f698ac3df75d537fda
Jose <jmlb0003@gmail.com>
2018-08-05 03:39:40 +0200

    Created an extension and values to identify tablets

--- app/src/main/java/com/jmlb0003/bbcnews/utils/Extensions.kt
+++ app/src/main/java/com/jmlb0003/bbcnews/utils/Extensions.kt
@@ -1,10 +1,20 @@
 package com.jmlb0003.bbcnews.utils
 
+import android.content.res.Resources
 import android.support.annotation.LayoutRes
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.jmlb0003.bbcnews.R
 
 fun ViewGroup.inflate(@LayoutRes layoutResourceId: Int, attachToRoot: Boolean = false): View {
     return LayoutInflater.from(context).inflate(layoutResourceId, this, attachToRoot)
+}
+
+internal fun Resources.isForTablet(): Boolean {
+    return try {
+        this.getBoolean(R.bool.is_tablet)
+    } catch (ex: Resources.NotFoundException) {
+        false
+    }
 }
