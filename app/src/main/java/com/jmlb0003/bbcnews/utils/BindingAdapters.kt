84778480e2eec4b1c30a2a76fa45a7672980aee5
Jose <jmlb0003@gmail.com>
2018-08-05 12:44:51 +0200

    Added databinding for news items in the list

--- app/src/main/java/com/jmlb0003/bbcnews/utils/BindingAdapters.kt
+++ app/src/main/java/com/jmlb0003/bbcnews/utils/BindingAdapters.kt
@@ -4,6 +4,7 @@ import android.databinding.BindingAdapter
 import android.support.v7.widget.RecyclerView
 import android.view.View
 import android.webkit.WebView
+import android.widget.ImageView
 import com.jmlb0003.bbcnews.domain.model.NewsItem
 import com.jmlb0003.bbcnews.presentation.news.adapter.NewsAdapter
 
@@ -25,3 +26,6 @@ internal fun setNewsDetail(webView: WebView, newsLink: String?) {
 internal fun setVisibility(view: View, visible: Boolean) {
     view.visibility = if (visible) View.VISIBLE else View.GONE
 }
+
+@BindingAdapter("imageSrc")
+internal fun setImageResource(imageView: ImageView, imageUrl: String?) {}
